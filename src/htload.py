2c212d3a45f610abd661cb5c6fcb8a7f822cee6b
Agata Filho <agata78@gmail.com>
2014-12-27 19:39:24 -0500

    Add simple HTML loader.
    The parsing is done by BeautifulSoup, but the result is a list of plain
    old Python objects -- strings and dictionaries -- rather than custom
    classes.

--- src/htload.py
+++ src/htload.py
@@ -1,27 +1,96 @@
-from bs4 import BeautifulSoup
+import bs4
 
-def loads(html):
-    """(str) -> object
+# TODO Is there a more Pythonic way of distinguishing tags, strings, comments,
+# etc., than checking exact classes?
 
-    Deserialize HTML, and return a Python object.
+def is_tag(x):
+    return bs4.element.Tag == type(x)
+
+def is_text(x):
+    return bs4.element.NavigableString == type(x)
+
+def children_of(tag):
+    """(bs4.BeautifulSoup or bs4.element.Tag) -> list
+
+    Return a list of Python objects representing the children of `tag`.
+    """
+    return [from_soup(child) for child in tag]
+
+def from_tag(tag):
+    """(bs4.element.Tag) -> dict"""
+    return {
+        "tag"         : tag.name,
+        "attributes"  : tag.attrs,
+        "children"    : children_of(tag)
+    }
+
+def from_soup(element):
+    """(bs4 element) -> str or dict
+
+    Tag elements are converted to Python dictionaries.  NavigableString
+    elements are converted to Python strings.  All other element types are
+    converted to empty strings.
+    """
+    if is_tag(element):
+        return from_tag(element)
+    elif is_text(element):
+        return str(element)
+    else:
+        return ""
+
+def coalesce_text(nodes):
+    """(list) -> list
+
+    Returns the result of replacing each sequence of consecutive strings in
+    `nodes` with a single string.  Does not modify `nodes`.
     """
     pass # TODO
 
-def load(file):
-    """(stream) -> object
+def deep_compare(xs, ys):
+    if type(xs) != type(ys):
+        return False
+    elif type(xs) == str:
+        return xs == ys
+    elif type(xs) == list:
+        same_length = len(xs) == len(ys)
+        return same_length and all(deep_compare(x, y) for x, y in zip(xs, ys))
+    elif type(xs) == dict:
+        keys = sorted(xs.keys())
+        same_keys = keys == sorted(ys.keys())
+        return same_keys and all(xs[k] == ys[k] for k in keys)
+    else:
+        raise "unexpected type"
 
-    Deserialize HTML from an input stream, and return a Python object.
+def loads(html):
+    """(str) -> list
+
+    Deserialize HTML, and return a list of strings and dicts.
+    """
+    return children_of(bs4.BeautifulSoup(html))
+
+def load(file):
+    """(stream) -> list
+
+    Deserialize HTML from a stream, and return a list of strings and dicts.
     """
     return loads(file.read())
+
+def loadf(fname):
+    """(str) -> list
+
+    Return strings and dicts representing the HTML contents of the named file.
+    """
+    with open(fname) as file:
+        return load(file)
 
 if __name__ == '__main__':
 
     expected = [
-        {   "tag"           : "hl",
+        {   "tag"           : "h1",
             "attributes"    : { },
             "children"      : [ "Algorithm Requirements" ]
         },
-        "\n\n",
+        "\n", "", "\n",
         {   "tag"           : "p",
             "attributes"    : { },
             "children"      : [
@@ -36,31 +105,31 @@ if __name__ == '__main__':
                 ":"
             ]
         },
-        "\n\n",
+        "\n",   # TODO Work around BeautifulSoup whitespace compression: "\n\n"
         {   "tag"           :  "ol",
-            "attributes"    : { },
+            "attributes"    : { "class": ["big", "bad"] },
             "children"      : [
-                "\n  ",
+                "\n",
                 {   "tag"           : "li",
                     "attributes"    : { },
                     "children"      : [ "Finiteness" ]
                 },
-                "\n  ",
+                "\n",
                 {   "tag"           : "li",
                     "attributes"    : { },
                     "children"      : [ "Definiteness" ]
                 },
-                "\n  ",
+                "\n",
                 {   "tag"           : "li",
                     "attributes"    : { },
                     "children"      : [ "Input" ]
                 },
-                "\n  ",
+                "\n",
                 {   "tag"           : "li",
                     "attributes"    : { },
                     "children"      : [ "Output" ]
                 },
-                "\n  ",
+                "\n",
                 {   "tag"           : "li",
                     "attributes"    : { },
                     "children"      : [ "Correctness" ]
@@ -71,7 +140,6 @@ if __name__ == '__main__':
         "\n"
     ]
 
-    with open("test.html") as file:
-        actual = load(file)
+    actual = loadf("test.html")
 
-    assert(expected == actual)
+    assert(deep_compare(expected, actual))
